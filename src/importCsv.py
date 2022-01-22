c7c4098513f5bc25496a7f9366e9ce7f423d86cc
sawasick <50393229+sawasick@users.noreply.github.com>
2022-01-22 17:09:18 +0900

    add&update:#7 テスト用のcsvを追加

--- src/importCsv.py
+++ src/importCsv.py
@@ -1,3 +1,8 @@
+'''
+コマンドラインから引数でcsvファイルを読み込み、配列に格納する
+python src/importCsv.py test.csv
+'''
+
 # sysモジュールでコマンドライン引数のリスト取得
 import sys
 
@@ -6,15 +11,34 @@ import csv
 
 data = [] # csvデータを格納する配列
 
+'''
+読み込むcsvは下記の形式
+URL,
+精度,
+閲覧時のviewportの幅,閲覧時のviewportの高さ,
+閲覧時間,
+posX,posY,
+...
+
+このままだとカンマの後に空文字列が入るので行の末尾の要素を削除する処理をする→row.pop(-1)
+'''
 def OpenCsv(arg):
     filename =  arg
     with open(filename, encoding='utf8', newline='') as f:
         csvreader = csv.reader(f)
         for row in csvreader:
+            row.pop(-1) # 上記のcsvの扱い方参照
+            # print(row)
             data.append(row)
             # for d in row:
                 # print(d)
-        print(data)
+        # print(data)
+        # print(data[0]) # URL
+        # print(data[1]) # 精度
+        # print(data[2][0]) # 閲覧時のviewportの幅
+        # print(data[2][1]) # 閲覧時のviewportの高さ
+        # print(data[4以降][0]) # posX
+        # print(data[4以降][1]) # posY
 
 # filename = './test.csv'
 args = sys.argv
