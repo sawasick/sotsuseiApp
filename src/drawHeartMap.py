ca6a4cf3dd8a745843f09d5254cf7342759c4163
sawasick <50393229+sawasick@users.noreply.github.com>
2022-01-23 11:58:53 +0900

    update:#9 プロットデータからヒートマップ画像作成機能完成

--- src/drawHeartMap.py
+++ src/drawHeartMap.py
@@ -1,47 +1,5 @@
 from PIL import Image, ImageDraw
 import random
 
-# ベタ画像作成, 引数は(モード、サイズ、塗りつぶす色)
-img = Image.new('RGBA', (500, 500))
+img = Image.new('RGBA', (5, 10)) # captureWebPage.pyから幅と高さの値持ってくる
 draw = ImageDraw.Draw(img)
-
-# 1ピクセルの点を描画
-# draw.point((350, 200), fill=(255, 255, 0))
-
-# 1点1点ランダムな色で描画
-for i in range(500):
-    for j in range(500):
-        draw.point((i, j), fill=(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
-img.save('draw.png', quality=95)
-
-# ヒートマップのイメージ
-img2 = Image.new('RGBA', (500, 500))
-draw2 = ImageDraw.Draw(img2)
-for i in range(100):
-    for j in range(100):
-        draw2.point((100+i, 100+j), fill=(255, 255, 200))
-for i in range(100):
-    for j in range(100):
-        draw2.point((200+i, 100+j), fill=(255, 255, 0))
-for i in range(100):
-    for j in range(100):
-        draw2.point((300+i, 100+j), fill=(255, 255, 200))
-for i in range(100):
-    for j in range(100):
-        draw2.point((100+i, 200+j), fill=(255, 255, 0))
-for i in range(100):
-    for j in range(100):
-        draw2.point((200+i, 200+j), fill=(255, 0, 0))
-for i in range(100):
-    for j in range(100):
-        draw2.point((300+i, 200+j), fill=(255, 255, 0))
-for i in range(100):
-    for j in range(100):
-        draw2.point((100+i, 300+j), fill=(255, 255, 200))
-for i in range(100):
-    for j in range(100):
-        draw2.point((200+i, 300+j), fill=(255, 255, 0))
-for i in range(100):
-    for j in range(100):
-        draw2.point((300+i, 300+j), fill=(255, 255, 200))
-img2.save('draw2.png', quality=95)
